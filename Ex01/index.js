80ec6ac1f601ab8d43a6477d280b3079ae41cb9b
LeonardoCMelo <leonardoc.melo2002@gmail.com>
2022-11-22 17:22:24 -0300

    added save button and basic functions

--- Ex01/index.js
+++ Ex01/index.js
@@ -1,3 +1,10 @@
-// document.getElementById("count").innerText = 2;
-
 let count = 0
+
+function increment_clk() {
+    count+=1
+    document.getElementById("counter").innerText = count
+}
+
+function save_clk(){
+    document.getElementById("save-paragraph").innerText = document.getElementById("save-paragraph").innerText + document.getElementById("counter").innerText + " - "
+}
